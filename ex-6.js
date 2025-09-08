9c9c05976272bbe9533f1745b52b0376f1b1cb5e
Supapond <supapondmaliwab@gmail.com>
2025-09-08 17:06:34 +0700

    fead(ex-6):add code

--- ex-6.js
+++ ex-6.js
@@ -2,5 +2,8 @@
 
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let concatSentence = "Total: " + totalParts + " Machine parts";
+console.log(concatSentence);
+
+let literalSentence = `Total: ${totalParts} Machine parts`;
+console.log(literalSentence);
