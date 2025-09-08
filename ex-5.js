d6f5e5f21060c1488bc240ef6ba1967e6bb9512a
Supapond <supapondmaliwab@gmail.com>
2025-09-08 17:02:33 +0700

    fead(ex-5):add code

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = (totalPrice * 25) / 100;
+let total = totalPrice - discount; 
+
+console.log(total);
